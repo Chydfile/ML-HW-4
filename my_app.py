33acf5335210ae1cde33a0a92a9c01fbb8fe1eb3
Chydfile <48090062+Chydfile@users.noreply.github.com>
2020-12-25 00:50:53 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,5 +1,6 @@
 import pickle
 import catboost
+import streamlit as st
 import sys
 import numpy as np
 from catboost import CatBoostRegressor
@@ -8,8 +9,8 @@ res = []
 with open('logisticRegr.pkl', 'rb') as f:
     model = pickle.load(f)
 for i in range(6):
-    res.append(sys.stdin.readline().strip())
+    res.append(st.write())
 
 x_arr = np.array(res)
 y = np.expm1(model.predict(x_arr))
-print(y)
+st.text_area(y)
