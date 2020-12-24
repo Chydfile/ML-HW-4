b399b34a06b9b2a37fbcbddc2a437eb4a009ad53
Chydfile <48090062+Chydfile@users.noreply.github.com>
2020-12-25 01:15:25 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -8,7 +8,12 @@ st.title('My first app')
 res = []
 with open('logisticRegr.pkl', 'rb') as f:
     model = pickle.load(f)
-res.append(st.text_area('name'))
+res.append(st.text_area('name1'))
+res.append(st.text_area('name2'))
+res.append(st.text_area('name3'))
+res.append(st.text_area('name4'))
+res.append(st.text_area('name5'))
+res.append(st.text_area('name6'))
 x_arr = np.array(res)
 y = np.expm1(model.predict(x_arr))
 st.write(y)
