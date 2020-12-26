0b64ad1d7d6f3d56d5474e2ea1bb15fb18e9d537
Chydfile <48090062+Chydfile@users.noreply.github.com>
2020-12-26 13:51:13 +0300

    Update to_verify.py

--- to_verify.py
+++ to_verify.py
@@ -13,3 +13,8 @@ if agree:
 genre = st.radio(
 "What's your favorite movie genre",
 ('Comedy', 'Drama', 'Documentary'))
+
+if genre == 'Comedy':
+    st.write('You selected comedy.')
+else:
+st.write("You didn't select comedy.")
