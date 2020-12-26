3075a37488bb41830c9938cbc0b9799bf4e4fcd1
Chydfile <48090062+Chydfile@users.noreply.github.com>
2020-12-26 14:28:05 +0300

    Update to_verify.py

--- to_verify.py
+++ to_verify.py
@@ -32,3 +32,15 @@ add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
     ("Email", "Home phone", "Mobile phone")
 )
+
+
+col1, col2, col3 = st.beta_columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", use_column_width=True)
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", use_column_width=True)
+with col3:
+    st.image("https://static.streamlit.io/examples/owl.jpg", use_column_width=True)
